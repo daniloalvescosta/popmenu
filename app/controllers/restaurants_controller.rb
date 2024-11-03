78b2bd60556f73bf9bc2055a234cefa43cbc7a62
Danilo Costa <danilo.costa25932593@gmail.com>
2024-11-03 12:50:33 -0300

    removed commentaries

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,19 +1,14 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: %i[ show update destroy ]
 
-  # GET /restaurants
-  # GET /restaurants.json
   def index
     @restaurants = Restaurant.all
   end
 
-  # GET /restaurants/1
-  # GET /restaurants/1.json
   def show
   end
 
-  # POST /restaurants
-  # POST /restaurants.json
+
   def create
     @restaurant = Restaurant.new(restaurant_params)
 
@@ -24,8 +19,7 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /restaurants/1
-  # PATCH/PUT /restaurants/1.json
+
   def update
     if @restaurant.update(restaurant_params)
       render :show, status: :ok, location: @restaurant
@@ -34,19 +28,16 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  # DELETE /restaurants/1
-  # DELETE /restaurants/1.json
+
   def destroy
     @restaurant.destroy!
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def restaurant_params
       params.require(:restaurant).permit(:name, :image_url)
     end
